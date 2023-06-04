477c8492141df467d8b49ec8918489ebd0bcf255
Nishant Bhalothia <nishantbhalothia444@gmail.com>
2023-06-04 11:21:29 +0530

    updated .create module

--- codeial/controller/users_controller.js
+++ codeial/controller/users_controller.js
@@ -33,6 +33,13 @@ module.exports.create = async (req, res)=>{
     if(req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
+    let user =await User.findOne({email: req.body.email});
+    // if userr already exist then redirect to sign in page
+    if(user){
+        // return res.redirect('back');
+        return res.redirect('/users/sign-in');
+    }
+    // if user not exist then create user
     await User.create(req.body);
     return res.redirect('/users/sign-in');
 
@@ -43,21 +50,15 @@ module.exports.create = async (req, res)=>{
 //     if(req.body.password != req.body.confirm_password){
 //         return res.redirect('back');
 //     }
-//     User.findOne({email: req.body.email}, (err, user)=>{
-//         if(err){
-//             console.log('error in finding user in signing up');
-//             return;
-//         }
+//     User.findOne({email: req.body.email}).exec(function(err, user){
+//         if(err){console.log('error in finding user in signing up'); return}
+
 //         if(!user){
-//             User.create(req.body, (err, user)=>{
-//                 if(err){
-//                     console.log('error in creating user while signing up');
-//                     return;
-//                 }
-//                 return res.redirect('/users/sign-in');
-//             })
+//             User.create(req.body);
+//             return res.redirect('/users/sign-in');
 //         }else{
 //             return res.redirect('back');
 //         }
 //     })
+
 // }

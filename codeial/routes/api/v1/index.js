25e6267cddcc9dafbd63d3720f41bdbf2c830dda
Nishant Bhalothia <nishantbhalothia444@gmail.com>
2023-07-16 19:17:01 +0530

    added passport jwt routes

--- codeial/routes/api/v1/index.js
+++ codeial/routes/api/v1/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 console.log('Api V1 index Route got kiccked in')
 
 
-// router.use('/users', require('./users.js'));
+router.use('/users', require('./users.js'));
 router.use('/posts', require('./posts'));
 // router.use('/comments', require('./comments.js'));
 
